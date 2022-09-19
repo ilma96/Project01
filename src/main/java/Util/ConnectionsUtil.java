06273ec5fc2523aaf502128ef650136fcc9137eb
Ilma Shaharin <shaharinilma96@gmail.com>
2022-09-18 22:58:04 -0400

    connected to Azure SQL server

--- src/main/java/Util/ConnectionsUtil.java
+++ src/main/java/Util/ConnectionsUtil.java
@@ -9,9 +9,9 @@ public class ConnectionsUtil {
     public static Connection getConnection(){
         if(c == null){
             try{
-                String urlSQLServer = "jdbc:sqlserver://localhost:1433;TrustServerCertificate=True";
-                String username = "sa";
-                String password = "P@SSWORD123";
+                String urlSQLServer = "jdbc:sqlserver://foodmaniaserver.database.windows.net:1433;database=dbfood;user=foodmania@foodmaniaserver;password={your_password_here};encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+                String username = System.getenv("dbusername");
+                String password = System.getenv("dbpassword");
                 c = DriverManager.getConnection(urlSQLServer, username, password);
             }catch(SQLException se){
                 se.printStackTrace();
